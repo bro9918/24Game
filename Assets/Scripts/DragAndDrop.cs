cf5b737c16292b9b2b88542d7c79e7a41c6d8270
Bryan O'Neill <bro9918@g.rit.edu>
2014-10-13 21:41:24 -0400

    Added main scene, prefabs, textures, operators, operator boxes, and drag and drop script. The snap function is not working properly at the moment and so it has been commented out for the time being.

---
+++ Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragAndDrop : MonoBehaviour {
+
+	private Vector3 mousePos;
+	private bool dragging;
+	private bool snap;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+	}
+
+	void OnMouseDrag () {
+		mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		if(this.tag == "Operator")// && snap == false)
+		{
+			this.transform.position = new Vector3(mousePos.x, mousePos.y, -1.0f);
+			dragging = true;
+		}
+		else
+			dragging = false;
+
+		if(dragging == true)
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			
+			if(Physics.Raycast (ray, out hit))
+			{
+				if(hit.transform.gameObject.tag == "OperatorBox")
+				{
+					this.transform.position = new Vector3(hit.transform.position.x, hit.transform.position.y, -1.0f);
+					snap = true;
+				}
+				else if(hit.transform.gameObject.tag == "Unsnap")
+					snap = false;
+			}
+		}
+	}
+}
