4cec66cec1e9125c2d8998d8978c8d583b91cfcd
Bryan O'Neill <bro9918@g.rit.edu>
2014-10-19 17:20:22 -0400

    Minor changes to ingredients screen

--- Assets/Scripts/SelectIngredients.cs
+++ Assets/Scripts/SelectIngredients.cs
@@ -3,13 +3,21 @@ using System.Collections;
 
 public class SelectIngredients : MonoBehaviour {
 
+	public int maxIngredients = 2;
+	private GameObject cuttingBoard;
+	private DragAndDrop dragAndDrop;
+
 	// Use this for initialization
 	void Start () {
-	
+		cuttingBoard = GameObject.FindGameObjectWithTag("Cutting Board");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void OnMouseUp () {
+
 	}
 }
